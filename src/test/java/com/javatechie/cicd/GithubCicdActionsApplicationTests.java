af7dbf709fde8d1ee77b3a5dada2375bb8c65539
rahuls512 <rahulsharan512@gmail.com>
2023-10-07 16:00:46 +0530

    code

--- src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
+++ src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.rahulsharan.java_app;
+package com.javatechie.cicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaAppApplicationTests {
+class GithubCicdActionsApplicationTests {
 
 	@Test
 	void contextLoads() {
